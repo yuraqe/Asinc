6d8bb1f791a13f58d7032f5b94c904fee79beef9
Jurij Bekasov <treasure.yura@gmail.com>
2024-12-21 16:53:41 +0200

    added switch to alert and more CSS celectors.

--- pars.py
+++ pars.py
@@ -1,17 +1,35 @@
-import time
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-#chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_argument('--headless')
 
-url = 'https://parsinger.ru/selenium/1/1.html'
 
-profile_params = ['MAX', 'PETROV', 'divma', '12', 'samara', 'per@pr.ru']
-with webdriver.Chrome() as browser:
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+
+url = 'https://parsinger.ru/selenium/5.5/5/1.html'
+
+with webdriver.Chrome(options=chrome_options) as browser:
     browser.get(url)
-    input_form = browser.find_elements(By.CLASS_NAME, 'form')
-    for i, el in enumerate(input_form):
-        el.send_keys(profile_params[i])
-    browser.find_element(By.ID, 'btn').click()
-    print(browser.find_element(By.ID, 'result').text)
+    elems = browser.find_elements(By.CSS_SELECTOR,'div[style]')[1:]
+    for elem in elems:
+        color_hex = elem.find_element(By.CSS_SELECTOR, 'span').text
+        res_btn = elem.find_elements(By.CSS_SELECTOR, 'option[value]')
+        for res in res_btn:
+            if res.text.strip() == color_hex.strip():
+                res.click()
+        button_color = elem.find_elements(By.CSS_SELECTOR, 'button[data-hex]')
+        for btn in button_color:
+            if btn.get_attribute('data-hex').strip() == color_hex:
+                btn.click()
+        elem.find_element(By.CSS_SELECTOR, 'input[type="checkbox"]').click()
+        elem.find_element(By.CSS_SELECTOR, 'input[type="text"]').send_keys(color_hex)
+        elem.find_element(By.XPATH, "//button[text()='Проверить']").click()
+
+
+    button = browser.find_element(By.XPATH, "//button[text()='Проверить все элементы']")
+    button.click()
+    sleep(2)
+    alert = browser.switch_to.alert
+    alert_text = alert.text
+    print(alert_text)
